64a19cea7f548bffa05c2b761473d92565a56950
Sam Slotsky <saxosamo@gmail.com>
2016-11-23 22:25:14 -0600

    hookup devtools

--- app/Main.jsx
+++ app/Main.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
 
 import reducer from './reducer'
 
 import App from './App'
+import DevTools from './DevTools'
 import ReduxTroll from './ReduxTroll'
 
-const store = createStore(reducer)
+const store = createStore(reducer, compose(DevTools.instrument()))
 
 ReactDOM.render((
   <Provider store={store}>
     <div>
-      <ReduxTroll />
+      <DevTools />
       <App />
     </div>
   </Provider>
