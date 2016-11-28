35c61ee4157b9e1f64f6f7a3aa21e8e78837de9d
Sam Slotsky <saxosamo@gmail.com>
2016-11-27 21:57:39 -0600

    more legwork

--- app/App.jsx
+++ app/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react'
 import TodoApp from './TodoApp'
 
+import '../styles.scss'
+
 export default class App extends Component {
   render() {
     return (
